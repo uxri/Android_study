b4a6f8513a40690dd91a4491fdbd2ada94ebe20b
uxri <yuree0217@g.eulji.ac.kr>
2022-07-07 16:30:41 +0900

    [FEATURE] Activity slide up, down animation

--- app/src/main/java/com/example/android_study/presentation/MainActivity.kt
+++ app/src/main/java/com/example/android_study/presentation/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.android_study.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.android_study.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,4 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun goToSecondActivity(view: View?) {
+        val intent = Intent(this@MainActivity, SecondActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(R.anim.slide_up, R.anim.slide_down)
+    }
+
 }
